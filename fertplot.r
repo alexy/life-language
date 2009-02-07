9dc1773804537b204020f601fbd1bc612df51dcc
Alexy Khrabrov <deliverable@gmail.com>
2009-02-06 21:33:13 -0500

    eps figure for kdd 2009!

--- fertplot.r
+++ fertplot.r
@@ -1,3 +1,4 @@
+#!/usr/bin/env r
 a <- list()
 ia <- 0
 for (i in 1:97) { 
@@ -21,6 +22,10 @@ xs.max.i <- which(xs==xs.max)[1]
 a.max <- a[[xs.max.i]]
 a.rest <- a[-xs.max.i]
 
-plot(a.max,type="l")
+postscript("sensor-fertility.eps",horizontal=F,onefile=F,height=3.5,width=3.2)
+plot(a.max,type="l",ann=F)
 #devnull <- sapply(a.rest,lines)
 devnull <- Map(lines,a.rest)
+title(main="Fertility of MIT Reality")
+title(xlab="total words", ylab="total new words so far")
+dev.off()
